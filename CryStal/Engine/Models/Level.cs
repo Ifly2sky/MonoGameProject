f2989c3dddf7c8b440dd488dd2e8b057f09c732e
Ifly2sky <tinhau24@gmail.com>
2023-02-16 21:11:58 +0200

    added level.cs with some basic variables

---
+++ CryStal/Engine/Models/Level.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryStal.Engine.Models
+{
+    public class Level
+    {
+        private int[,] _levelTiles;
+        string _levelPath;
+
+        public int[,] LevelTiles
+        {
+            get { return _levelTiles; }
+            set { _levelTiles = value; }
+        }
+
+        public Level(string path)
+        {
+            _levelPath = path;
+        }
+    }
+}
