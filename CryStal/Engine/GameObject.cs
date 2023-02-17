38e3ffaba2bc6a9514ac5ab31b88ca324aa77300
Ifly2sky <tinhau24@gmail.com>
2023-02-18 01:26:09 +0200

    added some classes for physics and added plans for level system

---
+++ CryStal/Engine/GameObject.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryStal.Engine
+{
+    public class GameObject
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Size { get; set; }
+
+    }
+}
---
+++ CryStal/Engine/Physics.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryStal.Engine
+{
+    public class Physics
+    {
+        const float half = 0.5f;
+
+        public static void CheckCollition(GameObject obj, GameObject target)
+        {
+            Vector2 objCenter = obj.Size * half;
+            Vector2 targetCenter = target.Size * half;
+
+            Vector2 distance = obj.Position + targetCenter - target.Position + targetCenter;
+            Vector2 bounds = targetCenter + objCenter;
+
+            if(distance.X < bounds.X && distance.Y < bounds.Y)
+            {
+                obj.Position -= bounds - distance;
+            }
+        }
+    }
+}
