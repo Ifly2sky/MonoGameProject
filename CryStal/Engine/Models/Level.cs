5cb0702590dd2aea5beec136b82349a9e191b698
Ifly2sky <tinhau24@gmail.com>
2023-02-18 14:32:56 +0200

    Made Level able to load

--- CryStal/Engine/Models/Level.cs
+++ CryStal/Engine/Models/Level.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mime;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +14,87 @@ namespace CryStal.Engine.Models
 {
     public class Level : IDisposable
     {
+        public Tile[,] tiles;
+        public string levelPath = @"C:\Users\Miko\source\repos\CryStal\CryStal\Content\";
+        public List<Texture2D> textures = new List<Texture2D>();
+
+        public ContentManager Content 
+        {
+            get { return content; }
+        }
+        ContentManager content;
+
+        public int Width
+        {
+            get { return tiles.GetLength(0); }
+        }
+
+        public int Height
+        {
+            get { return tiles.GetLength(1); }
+        }
+
+        public Level(IServiceProvider serviceProvider, string fileName)
+        {
+            content = new ContentManager(serviceProvider, "Content");
+
+            LoadTextures();
+
+            LoadTiles(new FileStream(levelPath + fileName, FileMode.Open));
+        }
+
+        private void LoadTiles(FileStream fileStream)
+        {
+            int width;
+            List<string> lines = new List<string>();
+
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string line = reader.ReadLine();
+                width = line.Length;
+                while (line != null)
+                {
+                    lines.Add(line);
+                    if (line.Length != width)
+                        throw new Exception(String.Format("The length of line {0} is different from all preceeding lines.", lines.Count));
+                    line = reader.ReadLine();
+                }
+            }
+
+            tiles = new Tile[width, lines.Count];
+
+            for (int y = 0; y < Height; ++y)
+            {
+                for (int x = 0; x < Width; ++x)
+                {
+                    // to load each tile.
+                    char tileType = lines[y][x];
+                    tiles[x, y] = LoadTile(tileType);
+                }
+            }
+
+        }
+
+        private Tile LoadTile(char tiletype)
+        {
+            return tiletype switch
+            {
+                ' ' => new Tile(textures[0], CollitionType.Passable),
+                '#' => new Tile(textures[1], CollitionType.Passable),
+                _ => new Tile(textures[0], CollitionType.Passable),
+            };
+        }
+
+        public void LoadTextures()
+        {
+            textures.Add(null);
+            textures.Add(Content.Load<Texture2D>("Template"));
+        }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Content.Unload();
+            GC.SuppressFinalize(this);
         }
     }
 }
