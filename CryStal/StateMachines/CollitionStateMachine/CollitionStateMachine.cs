420016b7e37fd41a81297c54c38e73de77e843c0
tino hautanen <tinhau24@gmail.com>
2023-12-14 13:05:54 +0200

    added logic to the collition state machine

--- CryStal/StateMachines/CollitionStateMachine/CollitionStateMachine.cs
+++ CryStal/StateMachines/CollitionStateMachine/CollitionStateMachine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CryStal.Engine.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace CryStal.StateMachines.CollitionStateMachine
 {
-    internal class CollitionStateMachine
+    public class CollitionStateMachine
     {
+        private static ImpassableState impassableState = new ImpassableState();
+        private static PassableState passableState = new PassableState();
+        private static PlatformState platformState = new PlatformState();
+        private static SpikeState spikeState = new SpikeState();
+        private static ImpassableTileState impassableTileState = new ImpassableTileState();
+        private CollitionState currentState = passableState;
+
+        public void HandleCollition(GameObject obj, GameObject target)
+        {
+            currentState.HandleCollition(obj, target);
+        }
+        public void SetCollitonState(string CollitionType)
+        {
+            switch (CollitionType)
+            {
+                case "Passable":
+                    currentState = passableState;
+                    return;
+                case "Impassable":
+                    currentState = impassableState;
+                    return;
+                case "ImpassableTile":
+                    currentState = impassableTileState;
+                    return;
+                case "Platform":
+                    currentState = platformState;
+                    return;
+                case "Spike":
+                    currentState = spikeState;
+                    return;
+            }
+        }
     }
 }
